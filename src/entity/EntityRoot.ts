6859e50a55692a5560a8d079019b360ee83c120e
baofuchun <14569661+baofuchun@user.noreply.gitee.com>
2026-01-20 17:31:00 +0800

    补充根节点

--- src/entity/EntityRoot.ts
+++ src/entity/EntityRoot.ts
@@ -1,6 +1,7 @@
 // src/ecs/RootEntity.ts
 import { Engine } from "../engine/Engine.ts";
 import { Transform } from "../components/Transform.ts";
+import { EventComponent } from "../components/Event.ts";
 
 export class RootEntity {
     public readonly entityId: number;
@@ -8,12 +9,20 @@ export class RootEntity {
     constructor(private engine: Engine) {
         this.entityId = this.engine.ecs.createEntity();
         this.engine.ecs.addComponent(this.entityId, new Transform({ x: 0, y: 0 }));
+        this.engine.ecs.addComponent(this.entityId, new EventComponent());
     }
 
     /**
      * 获取 Transform 组件
      */
-    get transform(): Transform | undefined {
-        return this.engine.ecs.getComponent(this.entityId, Transform);
+    get transform(): Transform {
+        return this.engine.ecs.getComponent(this.entityId, Transform)!;
+    }
+
+    /**
+     * 获取 Event 组件
+     */
+    get event(): EventComponent {
+        return this.engine.ecs.getComponent(this.entityId, EventComponent)!;
     }
 }
